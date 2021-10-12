3b1d046bf98be10d1680d5d91b65fdb8603715fa
sleepyboy <sleepyboy123@hotmail.com>
2021-10-12 20:41:07 +0800

    Created Final Emails

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 
 ## Setup
 
+### Install Dependencies
+`pip install python-dotenv`
+
+### Split Huge Text File to Individual Mails
+`python email-parser.py`
+
+### Grade Individual Mail File
+`python email-grader.py`
+
+### Send Test Email
+`python email-sender.py`
+
--- email-sender.py
+++ email-sender.py
@@ -1,2 +1,36 @@
+import smtplib
+from dotenv import load_dotenv
+import os
+
+def send_email(eml_file, type):
+    smtp = None
+    with open(eml_file) as f:
+        message = f.read()
+    try:
+        if type == "hotmail":
+            smtp = smtplib.SMTP('smtp.live.com', 587)
+            smtp.ehlo()
+            smtp.starttls()
+            # Login Details stored in .env
+            smtp.login(os.environ.get('hotmail'), os.environ.get('h_password'))
+            smtp.sendmail('sleepyboy123@hotmail.com', 'sleepyboi234@hotmail.com', message)
+            print('Sent to Hotmail.')
+        if type == "gmail":
+            smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            smtp.ehlo()
+            # Login Details stored in .env
+            smtp.login(os.environ.get('gmail'), os.environ.get('g_password'))
+            smtp.sendmail('sleepytesting12345@gmail.com', 'sleepyboi1234@gmail.com', message)
+            print('Sent to Gmail.')
+    except Exception as e:
+        print('Failed to Send Mail.')
+        print(str(e))
+    finally:
+        if smtp != None:
+            smtp.close()
+
 if __name__ == "__main__":
-    print("Sending Emails")
+    load_dotenv()
+    send_email('hotmail_phishing.eml', 'hotmail')
+    send_email('google_phishing.eml', 'gmail')
+    
